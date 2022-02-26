a826cb6f764b4eebca232605054c9a2b8f90e632
Leeseunghwan7305 <sun1238799@naver.com>
2022-02-26 18:11:31 +0900

    댓글api 추가

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 import styles from "./Video.module.scss";
 import VideoList from "./VideoList";
@@ -9,6 +10,16 @@ type Props = {
 };
 const Video = ({ clickData, data, setClickData, tapToggle }: Props) => {
   let [subClickData, SetSubClickData] = useState<any[]>([]);
+  let [userComment, setUserComment] = useState<any[]>([]);
+  axios
+    .get(
+      `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=25&videoId=${
+        clickData.id.videoId ? clickData.id.videoId : clickData.id
+      }&key=${process.env.REACT_APP_Y0UTUBE_API_KEY}`
+    )
+    .then((result) => {
+      setUserComment(() => result.data.items);
+    });
   return (
     <div
       className={`${styles.video}  ${
@@ -26,9 +37,26 @@ const Video = ({ clickData, data, setClickData, tapToggle }: Props) => {
           height="600px"
         ></iframe>
         <h2>{clickData ? clickData.snippet.title : null}</h2>
-        <h3>{clickData ? clickData.snippet.channelTitle : null}</h3>
         <span>조회수:{subClickData && subClickData[0]}회</span>
         <hr />
+        <div className={styles.introduce}>
+          <img src={subClickData[1]}></img>
+          <h1>{subClickData[2]}</h1>
+        </div>
+        <hr />
+        <div>
+          {userComment.map((item) => (
+            <div className={styles.userComment}>
+              <img
+                src={item.snippet.topLevelComment.snippet.authorProfileImageUrl}
+              />
+              <div>
+                <p>{item.snippet.topLevelComment.snippet.textOriginal}</p>
+                <p>{item.snippet.topLevelComment.snippet.likeCount}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       <div className={styles.side}>
         {data.map((result) => {
