b545ac521c8bf5ce0d81ff8afbd14477bfb933f8
Leeseunghwan7305 <sun1238799@naver.com>
2022-03-14 22:04:14 +0900

    [ADD]background

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -7,6 +7,10 @@ type Props = {
 const Screen = ({ screenToggle, setScreenToggle }: Props) => {
   return (
     <div className={styles.screen}>
+      <div className={styles.box}></div>
+      <div className={styles.box2}></div>
+      <div className={styles.box3}></div>
+      <h1>Youtube에 오신걸 환영합니다</h1>
       <div className={styles.ClickCircle}>
         <button
           onClick={() => {
